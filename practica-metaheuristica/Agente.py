bdad3b01c0b788d00f7e6320458ea0c980c6d3e8
Ezequiel Raigorodsky <ezequielraigorodsky@gmail.com>
2021-08-19 09:48:02 -0300

    dando forma a clase de Agente

---
+++ practica-metaheuristica/Agente.py
@@ -0,0 +1,13 @@
+class Agente():
+    def __init__(self, symbol: str):
+        self.symbol = symbol
+    
+    def next_move(board: list(list)):
+        pass
+
+    def evaluate_board(board: list(list)):
+        pass
+
+    def make_decision():
+        pass
+
