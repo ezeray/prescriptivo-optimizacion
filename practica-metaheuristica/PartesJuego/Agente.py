7e501fb972ac778c0839ecdd62d359972b7079bd
Ezequiel Raigorodsky <ezequielraigorodsky@gmail.com>
2021-08-25 21:22:05 -0300

    agregando referee

--- practica-metaheuristica/PartesJuego/Agente.py
+++ practica-metaheuristica/PartesJuego/Agente.py
@@ -11,3 +11,22 @@ class Agente:
     def make_decision():
         pass
 
+
+# [
+#     ['X', '', 'X']
+#     ['', 'O', 'O']
+#     ['', '', '']
+# ]
+
+# poner como caracteristica si empece por esquina
+
+# f(x) = a1 * (empece en esquina) + a2 * () + a3 * ()
+
+# tendría por ejemplo:
+
+# f(x) = 0.6 * () + 0.1 * () + 2.7 * ()
+# f(x) = 0.6 * () + 0.1 * () + 2.4 * ()
+# f(x) = 0.4 * () + 0.7 * () + 0.1 * ()
+
+
+# no hace falts que las cotas exactas esten en un mismo rango, sino que lo que nos devuelva la funcion objetivo sea algo comparable
